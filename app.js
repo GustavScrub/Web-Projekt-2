2bca1d2e5bc07b2514a89439c7dea6de7fc06a62
Gustav Berg <berg.gustav@elev.ga.ntig.se>
2022-03-17 23:52:04 +0100

    final sync

--- app.js
+++ app.js
@@ -1,34 +1,69 @@
-    // selectar inputen med classen inputBOI
-    let theGodofNumbers = document.querySelector(".inputBOI");
-    // selectar h1 text med classen number
-    let daINPUT = document.querySelector(".number");
-    // gör en eventlistener som lyssnar för en change med en funktion
-    theGodofNumbers.addEventListener("change", () => {
+// selectar inputen med classen inputBOI
+let checkBox5 = document.querySelector(".checkBox5");
+let checkBox4 = document.querySelector(".checkBox4");
+let checkBox3 = document.querySelector(".checkBox3");
+let checkBox2 = document.querySelector(".checkBox2");
+let checkBox1 = document.querySelector(".checkBox1");
+// selectar h1 text med classen number
+let rank5 = document.querySelector(".rank5");
+let rank4 = document.querySelector(".rank4");
+let rank3 = document.querySelector(".rank3");
+let rank2 = document.querySelector(".rank2");
+let rank1 = document.querySelector(".rank1");
+// gör en eventlistener som lyssnar för en change med en funktion
+checkBox5.addEventListener("change", () => {
+  if (
+    // om det va true och hör en change blir de false och lägger till klassen hidden
+    checkBox5.getAttribute("data-active") === "true"
+  ) {
+    // gör det till false
+    checkBox5.setAttribute("data-active", "false");
+    // ger den klassen hidden
+    rank5.classList.add("hidden");
+  } else {
+    // gör det till true
+    checkBox5.setAttribute("data-active", "true");
+    // tar bort classen hidden
+    rank5.classList.remove("hidden");
+  }
+});
 
-        if
-        // om det va true och hör en change blir de false och lägger till klassen hidden
-        (theGodofNumbers.getAttribute("data-active") === "true")
-        {
-            // gör det till false
-            theGodofNumbers.setAttribute("data-active", "false");
-            // ger den klassen hidden
-            daINPUT.classList.add("hidden");
+checkBox4.addEventListener("change", () => {
+  if (checkBox4.getAttribute("data-active") === "true") {
+    checkBox4.setAttribute("data-active", "false");
+    rank4.classList.add("hidden");
+  } else {
+    checkBox4.setAttribute("data-active", "true");
+    rank4.classList.remove("hidden");
+  }
+});
 
+checkBox3.addEventListener("change", () => {
+  if (checkBox3.getAttribute("data-active") === "true") {
+    checkBox3.setAttribute("data-active", "false");
+    rank3.classList.add("hidden");
+  } else {
+    checkBox3.setAttribute("data-active", "true");
+    rank3.classList.remove("hidden");
+  }
+});
 
-        }
-        else
-        {
-            // gör det till true
-            theGodofNumbers.setAttribute("data-active", "true");
-            // tar bort classen hidden
-            daINPUT.classList.remove("hidden");
+checkBox2.addEventListener("change", () => {
+  if (checkBox2.getAttribute("data-active") === "true") {
+    checkBox2.setAttribute("data-active", "false");
+    rank2.classList.add("hidden");
+  } else {
+    checkBox2.setAttribute("data-active", "true");
+    rank2.classList.remove("hidden");
+  }
+});
 
-        }
-    }
-    )       
-
-//let currentPage = window.location.pathname.substring("/season".length, "/season".length + 1);
-
-//localStorage.setItem(`answer${currentPage}`, Math.PI);
-//let thingy = localStorage.getItem(`answer${currentPage}`);
-//console.log(thingy);
+checkBox1.addEventListener("change", () => {
+  if (checkBox1.getAttribute("data-active") === "true") {
+    checkBox1.setAttribute("data-active", "false");
+    rank1.classList.add("hidden");
+  } else {
+    checkBox1.setAttribute("data-active", "true");
+    rank1.classList.remove("hidden");
+  }
+});
